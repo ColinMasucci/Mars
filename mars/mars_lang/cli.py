11ebbfc881009b61f4954d1f1bd27766df1b47dc
ColinMasucci <colined14@gmail.com>
2026-04-14 22:24:49 -0400

    Updated Cli.py

--- mars/mars_lang/cli.py
+++ mars/mars_lang/cli.py
@@ -1,36 +1,45 @@
 import argparse
 from mars.mars_lang.interpreter import interpret_code_from_file
 from mars.mars_lang import ros_tools
-from mars.mars_lang.ros.ros_tools import list_topics
+
+VERSION = "0.1.0" #grab this version from the actual release once we have multiple on github
 
 def main():
     parser = argparse.ArgumentParser(prog="mars")
 
+    parser.add_argument("--version", action="store_true")
+
     sub = parser.add_subparsers(dest="command", required=True)
 
+    # run command
     run = sub.add_parser("run")
     run.add_argument("file")
 
-
+    # ros command
     ros = sub.add_parser("ros")
     ros_sub = ros.add_subparsers(dest="ros_command", required=True)
+
     ros_sub.add_parser("bridge")
 
     ros_topics = ros_sub.add_parser("topics")
     ros_topics.add_argument("--show")
     ros_topics.add_argument("--search")
 
+    # parse
     args = parser.parse_args()
 
+    # version override (before command execution)
+    if args.version:
+        print(f"mars {VERSION}")
+        return
+
+    # dispatch
     if args.command == "run":
         interpret_code_from_file(args.file)
 
     elif args.command == "ros":
         if args.ros_command == "bridge":
-            interpret_code_from_file(
-                "ros_stub.mars",
-                ros_autostart=True
-            )
+            interpret_code_from_file("ros_stub.mars", ros_autostart=True)
 
         elif args.ros_command == "topics":
             if args.show:
