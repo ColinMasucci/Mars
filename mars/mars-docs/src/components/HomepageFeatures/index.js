a54eb64165a70618b9ff0f4fb4ea7a7ddc2a27fd
ColinMasucci <colined14@gmail.com>
2026-04-29 16:50:42 -0400

    swapped back to svg
    there was a bug where the pngs werent showing due to some prebuilt react function I am not bothered to track down.

--- mars/mars-docs/src/components/HomepageFeatures/index.js
+++ mars/mars-docs/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Early Validation',
-    image: require('@site/static/img/compileCheck.png'),
+    Svg: require('@site/static/img/compileCheck.svg').default,
     description: (
       <>
         MARS detects hardware–software mismatches at compile time instead of runtime, preventing costly and hard-to-debug execution errors.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Hardware Abstraction',
-    image: require('@site/static/img/robotSwap.png'),
+    Svg: require('@site/static/img/robotSwap.svg').default,
     description: (
       <>
         MARS separates logic from physical components, allowing hardware (motors, sensors, etc.) to be swapped with minimal code changes.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Practical Integration',
-    image: require('@site/static/img/ros-python.png'),
+    Svg: require('@site/static/img/ros-python.svg').default,
     description: (
       <>
         Works alongside existing tools like ROS 2 and Python, making it usable in real-world robotics systems rather than purely theoretical.
@@ -32,11 +32,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({image, title, description}) {
+function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img src={image} className={styles.featureSvg} role="img" />
+        <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
