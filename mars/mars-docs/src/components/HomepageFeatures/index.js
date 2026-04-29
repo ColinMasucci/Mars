fbbb5f41f24761e78e0e36f1e02f0cb4ed61f5de
ColinMasucci <colined14@gmail.com>
2026-04-29 16:38:16 -0400

    Fixed switching to pngs from svgs react bug for vercel

--- mars/mars-docs/src/components/HomepageFeatures/index.js
+++ mars/mars-docs/src/components/HomepageFeatures/index.js
@@ -32,11 +32,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({image, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={image} className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
